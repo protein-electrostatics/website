bc44ff593a414f4b5b982961952b66181b693b15
pedrishi <pedro_reis_22@hotmail.com>
2020-02-13 14:43:03 +0000

    added privacy link & accommodation pages

--- _site/js/google-maps-hotels.js
+++ _site/js/google-maps-hotels.js
@@ -213,5 +213,40 @@ google.maps.event.addDomListener(window, "load", function () {
    * add markers to map
    */
 
+    var image = {
+        url: '', // image is 512 x 512
+        scaledSize : new google.maps.Size(50, 50),
+    };
+
+  var marker0 = createMarker({
+    position: new google.maps.LatLng(44.412823, 8.929982),
+    map: map,
+  }, "Hotel Cairoli ★★★<br/><a href='https://www.hotelcairoligenova.com/en/'>See More</a>");
+
+  var marker1 = createMarker({
+    position: new google.maps.LatLng(44.413490, 8.928708),
+    map: map,
+  }, "Hotel Helvetia ★★★<br/><a href='https://www.hotelhelvetiagenova.it/'>See More</a>");
+
+  var marker2 = createMarker({
+    position: new google.maps.LatLng(44.410022, 8.934915),
+    map: map,
+  }, "Best Western Hotel Metropoli ★★★<br/><a href='https://www.hotelmetropoli.it/'>See More</a>");
+
+  var marker3 = createMarker({
+    position: new google.maps.LatLng(44.411431, 8.928333),
+    map: map,
+  }, "Hotel De Ville ★★★★ <br/><a href='https://hoteldeville.it/'>See More</a>");
+
+  var marker4 = createMarker({
+    position: new google.maps.LatLng(44.406913, 8.936349),
+    map: map,
+  }, "Hotel Bristol Palace ★★★★<br/><a href='https://www.hotelbristolpalace.it/it'>See More</a>");
+
+  var marker5 = createMarker({
+    position: new google.maps.LatLng(44.416278, 8.923976),
+    map: map,
+  }, "Hotel Nuovo Nord  ★★★<br/><a href='https://www.hotelnuovonord.com/'>See More</a>");
+
 
 });
--- _site/js/google-maps.js
+++ _site/js/google-maps.js
@@ -14,7 +14,7 @@ google.maps.event.addDomListener(window, "load", function () {
   var map = new google.maps.Map(document.getElementById("map"), {
     center: new google.maps.LatLng(44.405882,8.9439043),
     zoom: 13,
-	  		
+
       // Google maps style
 
       styles: [
@@ -186,7 +186,7 @@ google.maps.event.addDomListener(window, "load", function () {
     ]
   }
 ],
-	  
+
     mapTypeId: google.maps.MapTypeId.ROADMAP
   });
 
@@ -212,16 +212,6 @@ google.maps.event.addDomListener(window, "load", function () {
   /*
    * add markers to map
    */
-    
-    var image = {
-        url: '', // image is 512 x 512
-        scaledSize : new google.maps.Size(50, 50),
-    };
-    
-  var marker0 = createMarker({
-    position: new google.maps.LatLng(37.219697, -8.544487),
-    map: map,
-    icon: image
-  }, "Dog Hotel Algarve <br/><a href='https://www.google.com/maps?saddr=My+Location&daddr=hotel+do+cao+algarve+portimao+portugal'>Get Directions</a>");
+
 
 });
